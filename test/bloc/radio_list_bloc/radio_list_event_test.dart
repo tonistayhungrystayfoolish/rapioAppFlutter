c9cc4434827406b2e34fd341eb899afee5aee879
alex-ia <alex@alex-ai.dev>
2026-03-31 21:44:20 +0200

    feat: KAN-4 - Creation Unit tests

--- test/bloc/radio_list_bloc/radio_list_event_test.dart
+++ test/bloc/radio_list_bloc/radio_list_event_test.dart
@@ -7,9 +7,8 @@ void main() {
       expect(LoadRadios.new, isNotNull);
     });
 
-    test('LoadRadios is a RadioListEvent', () {
-      final event = LoadRadios();
-      expect(event, isA<RadioListEvent>());
+    test('RadioListEvent is abstract', () {
+      expect(RadioListEvent, isA<Type>());
     });
   });
 
@@ -20,6 +19,11 @@ void main() {
     });
 
     test('LoadRadios can be instantiated', () {
+      expect(() => LoadRadios(), returnsNormally);
+    });
+
+    test('LoadRadios can be instantiated multiple times', () {
+      expect(() => LoadRadios(), returnsNormally);
       expect(() => LoadRadios(), returnsNormally);
     });
 
@@ -34,13 +38,13 @@ void main() {
       expect(event1, equals(event2));
     });
 
-    test('LoadRadios equality with different instances', () {
+    test('LoadRadios equality with different instances uses identity', () {
       final event1 = LoadRadios();
       final event2 = LoadRadios();
       expect(identical(event1, event2), isFalse);
     });
 
-    test('multiple LoadRadios instances are equal', () {
+    test('multiple LoadRadios instances are all LoadRadios type', () {
       final events = List.generate(5, (_) => LoadRadios());
       for (var event in events) {
         expect(event, isA<LoadRadios>());
@@ -51,6 +55,67 @@ void main() {
       final event1 = LoadRadios();
       final event2 = LoadRadios();
       expect(event1.runtimeType, equals(event2.runtimeType));
+    });
+
+    test('LoadRadios has correct runtime type name', () {
+      final event = LoadRadios();
+      expect(event.runtimeType.toString(), equals('LoadRadios'));
+    });
+
+    test('LoadRadios hashCode is consistent for same instance', () {
+      final event1 = LoadRadios();
+      expect(event1.hashCode, equals(event1.hashCode));
+    });
+
+    test('LoadRadios == returns true for same instance', () {
+      final event1 = LoadRadios();
+      final event2 = event1;
+      expect(event1 == event2, isTrue);
+    });
+
+    test('LoadRadios == returns false for different instances', () {
+      final event1 = LoadRadios();
+      final event2 = LoadRadios();
+      expect(event1 == event2, isFalse);
+    });
+
+    test('LoadRadios toString contains class name', () {
+      final event = LoadRadios();
+      final str = event.toString();
+      expect(str, contains('LoadRadios'));
+    });
+
+    test('LoadRadios is not equal to null', () {
+      final event = LoadRadios();
+      expect(event == null, predicate<bool>((value) => value == false));
+    });
+
+    test('LoadRadios is not equal to different type', () {
+      final event = LoadRadios();
+      expect(event == 'LoadRadios', predicate<bool>((value) => value == false));
+    });
+
+    test('LoadRadios string representation is valid', () {
+      final event = LoadRadios();
+      final str = event.toString();
+      expect(str, isNotEmpty);
+    });
+
+    test('LoadRadios can be used in set with identity', () {
+      final eventSet = <LoadRadios>{LoadRadios(), LoadRadios()};
+      expect(eventSet.length, equals(2));
+    });
+
+    test('LoadRadios can be compared with identical check', () {
+      final event1 = LoadRadios();
+      final event2 = LoadRadios();
+      expect(identical(event1, event2), isFalse);
+    });
+
+    test('LoadRadios instances have unique hashCodes', () {
+      final event1 = LoadRadios();
+      final event2 = LoadRadios();
+      expect(event1.hashCode, isNot(equals(event2.hashCode)));
     });
   });
 }
